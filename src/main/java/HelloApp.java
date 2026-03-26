3daa9fa049f62ba27054a184e608e21b2e720f2a
KLOKA-LEVITATION-69 <shivadityapandey7@gmail.com>
2026-03-26 12:37:49 +0530

    Update HelloApp.java

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,16 +1,15 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        // Case 1: No arguments
+
         if (args.length == 0) {
             System.out.println("Hello, World!");
             return;
         }
 
-        // Case 2: Join names directly
         String names = String.join(", ", args);
 
-        // Final output
+
         System.out.println("Hello, " + names + "!");
     }
 }
